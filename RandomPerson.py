29f0e86d8acd912c7986508d0666218d30f84316
HelloWorldCoder <HelloWorldCoder-China@outlook.com>
2026-09-25 14:55:31 +0800

    2.2-代码优化,大修复,彩蛋

--- RandomPerson.py
+++ RandomPerson.py
@@ -6,8 +6,9 @@ from tkinter import filedialog
 import shutil as sh
 import os
 import yaml
+from pathlib import Path
 main=tk.Tk()
-main.title("随机选人软件-初三4班出品 v2.1")
+main.title("随机选人软件-初三4班出品 v2.2")
 
 #region 配置管理
 class config:
@@ -48,6 +49,12 @@ class config:
                 else:
                     path_content[key]=path_new[key]
         searchreplace(self.content,content)
+        yaml.dump(self.content,open(self.name,"w+"))
+    def remove(self,key:list):
+        target=self.content
+        for i in key[:-1]:
+            target=target[i]
+        target.pop(key[-1])
         yaml.dump(self.content,open(self.name,"w+"))
 
 #endregion 配置管理
@@ -129,11 +136,12 @@ def numberrand():
             result.set("输入错误,请检查抽取数量与范围")
             return
         numbercfg.edit({"start":int(start.get()),"end":int(end.get())})
-        pool=ra.sample(range(int(start.get()),int(end.get())),int(count.get()))
-        result.set("")
+        pool=ra.sample(range(int(start.get()),int(end.get())+1),int(count.get()))
         for i in range(len(pool)):
             if i!=0:
                 result.set(result.get()+" ")
+            else:
+                result.set("")
             result.set(result.get()+str(pool[i]))
     except:
         result.set("输入错误,请检查抽取数量与范围")
@@ -161,24 +169,26 @@ def numbermode():
 
 #region 随机抽取名字
 def textrand():
-    global cfgname,namelist,count,result,startbtn,cfgnamerd
+    global cfgname,namelist,count,result,startbtn,cfgnamerd,namecfg
     try:
-        result.set("")
+        result.set("正在处理,请稍后......")
         if(len(namelist)<int(count.get())):
             result.set("输入错误,请检查抽取数量与范围")
             return
-        cfgnamerd["cfgname"]=cfgname.split("/")[-1].split(".")[0]
-        yaml.dump(cfgnamerd,open("./config/text.cfg","w+"))
+        namecfg.edit({"cfgname":cfgname})
+        choicelist=namelist.copy()
         for i in range(int(count.get())):
-            namenow=ra.choice(namelist)
-            namelist.remove(namenow)
+            namenow=ra.choice(choicelist)
+            choicelist.remove(namenow)
             if i!=0:
                 result.set(result.get()+" ")
+            else:
+                result.set("")
             result.set(result.get()+namenow)
     except Exception as e:
         result.set(e)
 def textmode():
-    global start,end,count,result,startbtn,modechooseframe,addboxregistry,cfgnamerd,cfgname,namelist,cfglistraw,textconfig,cfglist
+    global start,end,count,result,startbtn,modechooseframe,addboxregistry,cfgnamerd,cfgname,namelist,cfglistraw,textconfig,cfglist,namecfg
     for box in addboxregistry:
         box.destroy()
     addboxregistry.clear()
@@ -187,108 +197,86 @@ def textmode():
         global cfgname
         cfgname="选择列表文件"
     namecfg=config("./config/text.cfg",{"cfgname":"选择列表文件"},textcfgsetdefaultvalue)
-    else:
-        try:
-            cfgnamerd=yaml.load(cfg,Loader=yaml.SafeLoader)
-            if cfgnamerd["cfgname"] in open("./config/textlist/index.cfg","r+").read() and cfgnamerd["cfgname"]!="":
-                cfgname=cfgnamerd["cfgname"]
-            else:
-                cfgnamerd["cfgname"]="选择列表文件"
-                yaml.dump(cfgnamerd,open("./config/text.cfg","w+"))
-                cfgname="选择列表文件"
-        except:
-            cfgnamerd={"cfgname":"选择列表文件"}
-            yaml.dump(cfgnamerd,open("./config/text.cfg","w+"))
-            cfgname="选择列表文件"
-            print("配置文件损坏,已重置为默认配置")
-    
-    try:
-        cfg=open("./config/textlist/index.cfg","r+").read()
-    except:
-        cfg=""
-    if cfg=="":
-        cfglist=[""]
-        cfglistraw=[""]
-    else:
-        cfglist=cfg.split("\n")
-        try:
-            cfglist.remove("")
-        except:
-            pass
-        if cfglist==None or cfglist==[]:
-            cfglist=[""]
-        cfglistraw=cfglist.copy()
-        for i in range(len(cfglist)):
-            cfglist[i]=cfglist[i].split(".")[0]
+    cfgname=namecfg.content["cfgname"]
+
+    cfgindex=config("./config/textlist/index.cfg",{"index":{}})
+    cfglist=cfgindex.content["index"]
     namelist=[]
     def importcfg():
         global cfgname,textconfig
-        cfgname=filedialog.askopenfilename(title="选择名单文件",filetypes=[("名单列表(换行分割名字)","*")])
-        if cfgname.split("/")[-1].split(".")[0] in cfglist:
+        filename=filedialog.askopenfilename(title="选择名单文件",filetypes=[("名单列表(换行分割名字)","*")])
+        if not filename:
+            result.set("用户已取消导入")
+            return
+        source=Path(filename)
+        display_name=source.stem
+        stored_name=source.name
+        if display_name in cfglist:
             result.set("名单已存在,请勿重复导入")
             return
-        sh.copy(cfgname,"./config/textlist/"+cfgname.split("/")[-1])
-        open("./config/textlist/index.cfg","a+").write(cfgname.split("/")[-1]+"\n")
-        if cfglist==[""]:
-            try:
-                cfglist.remove("")
-            except:
-                pass
-        cfglist.append(cfgname.split("/")[-1].split(".")[0])
+        sh.copy(filename,"./config/textlist/"+stored_name)
+        cfgindex.edit({"index":{display_name:stored_name}})
+        optionlist=cfgindex.content["index"].keys()
+        cfgname=display_name
         textconfig.destroy()
-        textconfig=tk.OptionMenu(textconfigframe,tk.StringVar(value=cfgname.split("/")[-1].split(".")[0]),*cfglist)
+        textconfig=tk.OptionMenu(textconfigframe,tk.StringVar(value=cfgname),*optionlist,command=readcfg)
         textconfig.pack(side=tk.LEFT,before=textconfigimportbtn)
-        cfglistraw.append(cfgname.split("/")[-1])
-        cfgname=cfgname.split("/")[-1].split(".")[0]
         readcfg(cfgname)
     def removecfg():
         global cfgname,namelist,cfglistraw,textconfig,cfglist
         try:
             if cfgname!="选择列表文件":
-                for i in cfglistraw:
-                    if i.split(".")[0]==cfgname:
-                        cfgname=i
-                        break
-                open("./config/textlist/index.cfg","w+").write(open("./config/textlist/index.cfg","r+").read().replace(cfgname+"\n",""))
-                print(cfgname)
-                cfglist.remove(cfgname.split(".")[0])
-                if cfglist==None or cfglist==[]:
-                    cfglist=[""]
                 try:
-                    os.remove("./config/textlist/"+cfgname)
+                    removetarget=cfgindex.content["index"][cfgname]
+                    cfgindex.remove(["index",cfgname])
+                except:
+                    result.set("删除索引失败")
+                    ti.sleep(1)
+                try:
+                    os.remove("./config/textlist/"+removetarget)
+                    result.set("删除文件成功")
                 except:
                     pass
                 cfgname="选择列表文件"
+                namecfg.edit({"cfgname":cfgname})
+                namelist=["恭喜你触发了彩蛋!","Copyright ©️2026 HelloWorldCoder","https://github.com/HelloWorldCoder-China/RandomPerson","电教其实是福瑞","http://greatthings.dragonhwc.com"]
+                if not cfglist:
+                    optionlist=[""]
+                else:
+                    optionlist=cfgindex.content["index"].keys()
                 textconfig.destroy()
-                textconfig=tk.OptionMenu(textconfigframe,tk.StringVar(value=cfgname),*cfglist)
+                textconfig=tk.OptionMenu(textconfigframe,tk.StringVar(value=cfgname),*optionlist,command=readcfg)
                 textconfig.pack(side=tk.LEFT,before=textconfigimportbtn)
         except:
             result.set("删除失败,请检查文件是否存在")
     def readcfg(cfgnameinput):
-        global namelist,cfglistraw,cfgname
+        global namelist,cfgname
         cfgname=cfgnameinput
-        for i in cfglistraw:
-            if i.split(".")[0]==cfgnameinput:
-                cfgnameinput=i
-                break
+        cfgnameinput=cfgindex.content["index"][cfgnameinput]
         try:
-            cfg=open("./config/textlist/"+cfgnameinput,"r+").read()
+            with open("./config/textlist/"+cfgnameinput,"r+") as file:
+                cfg=file.read()
         except:
-            cfg=""
-        if cfg=="":
             result.set("名单文件不存在,请检查")
+            namelist=["恭喜你触发了彩蛋!","Copyright ©️2026 HelloWorldCoder","https://github.com/HelloWorldCoder-China/RandomPerson","电教其实是福瑞","http://greatthings.dragonhwc.com"]
             return
         namelist=cfg.split("\n")
         try:
             namelist.remove("")
         except:
             pass
-    if cfgname!="选择列表文件":
+    
+    if namecfg.content["cfgname"]!="选择列表文件":
+        cfgname=namecfg.content["cfgname"]
         readcfg(cfgname)
     
     textconfigframe=tk.Frame(main)
     textconfigframe.pack(pady=percentheight(2),after=modechooseframe)
-    textconfig=tk.OptionMenu(textconfigframe,tk.StringVar(value=cfgname),*cfglist,command=readcfg)
+    if not cfglist:
+        optionlist=[""]
+    else:
+        optionlist=cfgindex.content["index"].keys()
+    textconfig=tk.OptionMenu(textconfigframe,tk.StringVar(value=cfgname),*optionlist,command=readcfg)
     textconfig.pack(side=tk.LEFT)
     textconfigimportbtn=tk.Button(textconfigframe,text="导入",font=("微软雅黑",15),command=importcfg)
     textconfigimportbtn.pack(side=tk.LEFT)
@@ -312,15 +300,14 @@ modes=[
     "名单模式"
 ]
 def modesswitch(value):
-    global mode,swidth,cfgmain
+    global mode,swidth,maincfg
     if value=="座号模式":
         mode="number"
         numbermode()
     elif value=="名单模式":
         mode="text"
         textmode()
-    cfgmain["mode"]=mode
-    yaml.dump(cfgmain,open("./config/main.cfg","w+"))
+    maincfg.edit({"mode":mode})
 modechoosebox=tk.OptionMenu(modechooseframe,tk.StringVar(value=boxvalue),*modes,command=modesswitch)
 modechoosebox.pack()
 
